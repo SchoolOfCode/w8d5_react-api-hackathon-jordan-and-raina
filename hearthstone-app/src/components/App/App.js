c97a53b3bb5804ffce975b7ccd431e80342b1c48
Jordan Flash <flash.jordan.3@gmail.com>
2022-06-17 13:45:28 +0100

    test

--- hearthstone-app/src/components/App/App.js
+++ hearthstone-app/src/components/App/App.js
@@ -1,36 +1,45 @@
 import React, { useEffect, useState } from "react";
-import './App.css';
+import "./App.css";
 
-const testResults = [ {
-  "cardId":"TB_BaconShop_HERO_53",
-  "dbfId":"60370",
-  "name":"Ysera",
-  "cardSet":"Battlegrounds",
-  "type":"Hero",
-  "faction":"Neutral",
-  "health":40,
-  "playerClass":"Neutral",
-  "img":"https://d15f34w2p8l1cc.cloudfront.net/hearthstone/04015b4f36a332ad359d494e7cf095fc05e90f6f34487be0130b330631b38251.png",
-  "imgGold":"https://d15f34w2p8l1cc.cloudfront.net/hearthstone/ac4694f6807fa55c2698b0d7b00a325cc54528ba6525f8b18084e6222f233404.png",
-}];
+const testResults = [
+  {
+    cardId: "TB_BaconShop_HERO_53",
+    dbfId: "60370",
+    name: "Ysera",
+    cardSet: "Battlegrounds",
+    type: "Hero",
+    faction: "Neutral",
+    health: 40,
+    playerClass: "Neutral",
+    img: "https://d15f34w2p8l1cc.cloudfront.net/hearthstone/04015b4f36a332ad359d494e7cf095fc05e90f6f34487be0130b330631b38251.png",
+    imgGold:
+      "https://d15f34w2p8l1cc.cloudfront.net/hearthstone/ac4694f6807fa55c2698b0d7b00a325cc54528ba6525f8b18084e6222f233404.png",
+  },
+];
 
 function App() {
-  const [ results, setResults ] = useState(testResults);  
+  const [results, setResults] = useState(testResults);
 
-
+  useEffect(async () => {
+    const response = await fetch(
+      "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards"
+    );
+    const data = await response.json();
+    const [cards] = data.results;
+    setResults(cards);
+  });
 
   return (
     <div className="App">
-      
-    
-
+      <div id="ResultsList">
+        <p>{cards}</p>
+      </div>
     </div>
   );
 }
 
-// <SearchBar >  </SearchBar> // use this to assign a variable with search thing 
+// <SearchBar >  </SearchBar> // use this to assign a variable with search thing
 // search results use as prop?
-// function for each 
-
+// function for each
 
 export default App;

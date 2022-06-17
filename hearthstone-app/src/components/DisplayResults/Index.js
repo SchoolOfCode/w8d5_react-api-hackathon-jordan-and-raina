f8efb4bec4c3bf659c80e23b9a5cc882f5d6ee9e
chocorainaaa <blackettraina@gmail.com>
2022-06-17 15:45:16 +0100

    if statement added to display results component

--- hearthstone-app/src/components/DisplayResults/Index.js
+++ hearthstone-app/src/components/DisplayResults/Index.js
@@ -1,28 +1,28 @@
 import React from 'react'
 
-
- function DisplayResults({results}) {
-  console.log(results)
-    return (
-    <div>
-      <ul>
-      {results.map((item) => {
-        return (
-          <div>
-          {item.img? <img src ={item.img} alt={item.name}/> : <></> }
-          {item.img? <h4> {"Card Name: " + item.name}</h4> : <p></p> }
-          {item.img? <h4> {"Card Set: " + item.cardSet}</h4> : <p></p> }
-          {item.img? <h4> {"Type: " + item.type}</h4> : <p></p> }
-          {item.img? <h4> {"Faction: " + item.faction}</h4> : <p></p> }
-          {item.img? <h4> {"Health: " + item.health}</h4> : <p></p> }
-          {item.img? <h4> {"Player Class: " + item.playerClass}</h4> : <p></p> }
-          </div>
-        )
-      })
-      }
-      </ul>
-    </div>
-  )
-}
+function DisplayResults({results}) {
+    console.log(results)
+      return (
+      <div>
+        <ul>
+          {results.map((item) => {     
+              if (item.img){
+                  return (
+                  <div>
+                      <img src ={item.img} alt={item.name}/> 
+                      <h4> {"Card Name: " + item.name}</h4> 
+                      <h4> {"Card Set: " + item.cardSet}</h4>
+                      <h4> {"Type: " + item.type}</h4> 
+                      <h4> {"Faction: " + item.faction}</h4>
+                      <h4> {"Health: " + item.health}</h4>
+                      <h4> {"Player Class: " + item.playerClass}</h4>
+                  </div>)
+                  }    
+              })
+          }
+        </ul>
+      </div>
+    )
+  }
 
 export default DisplayResults;

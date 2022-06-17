9d4ca38e20b0d7691af0e41997404e36d2c1eb88
Jordan Flash <flash.jordan.3@gmail.com>
2022-06-17 15:33:58 +0100

    horizontal grid

--- hearthstone-app/src/components/DisplayResults/Index.js
+++ hearthstone-app/src/components/DisplayResults/Index.js
@@ -10,7 +10,12 @@ import React from 'react'
         return (
           <div>
           {item.img? <img src ={item.img} alt={item.name}/> : <></> }
-          {item.img? <h4> {item.name}</h4> : <p></p> }
+          {item.img? <h4> {"Card Name: " + item.name}</h4> : <p></p> }
+          {item.img? <h4> {"Card Set: " + item.cardSet}</h4> : <p></p> }
+          {item.img? <h4> {"Type: " + item.type}</h4> : <p></p> }
+          {item.img? <h4> {"Faction: " + item.faction}</h4> : <p></p> }
+          {item.img? <h4> {"Health: " + item.health}</h4> : <p></p> }
+          {item.img? <h4> {"Player Class: " + item.playerClass}</h4> : <p></p> }
           </div>
         )
       })
